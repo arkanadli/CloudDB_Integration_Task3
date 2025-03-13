487648f6ae4b24f19c29ff66453031121569c800
arkanadli <uzzomaki@gmail.com>
2025-03-13 17:54:09 +0700

    Update config.php

--- config.php
+++ config.php
@@ -5,8 +5,11 @@ $username = "arkanadli";
 $password = "adlidewa321123";
 
 try {
-    $pdo = new PDO("pgsql:host=$host;dbname=$dbname", $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $dsn = "pgsql:host=$host;dbname=$dbname;sslmode=require";
+    $pdo = new PDO($dsn, $username, $password, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    ]);
+
     echo "Koneksi ke PostgreSQL berhasil!";
 } catch (PDOException $e) {
     die("Koneksi gagal: " . $e->getMessage());
