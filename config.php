03b58fd0415a9f4bce7f86608dfd4ddd65d40fde
arkanadli <uzzomaki@gmail.com>
2025-03-13 17:27:38 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,13 +1,14 @@
 <?php
 $host = "database-1.cb44egeuga65.ap-southeast-2.rds.amazonaws.com";
-$dbname = "";
+$dbname = "nama_database_anda"; 
 $username = "arkanadli";
 $password = "adlidewa321123";
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo = new PDO("pgsql:host=$host;dbname=$dbname", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Koneksi ke PostgreSQL berhasil!";
 } catch (PDOException $e) {
-    die("Connection failed: " . $e->getMessage());
+    die("Koneksi gagal: " . $e->getMessage());
 }
 ?>
